d99b27546ab7021dd1e9202d20792ddbaee30c11
LoboStark <rocadie9615@ciencias.unam.mx>
2020-03-04 11:53:37 -0600

    Segundo Comit

--- Matriz.java
+++ Matriz.java
@@ -14,15 +14,29 @@ matriz solo se crean matrizes cuadradas
 Metodo con el que rellenas una matriz
 */
 public void rellenaMatriz(){
-  int[][] matriz= this.getMatriz()
+  int[][] matriz= this.getMatriz();
+  int columna=0;
+  int fila=0;
+  Scanner sc= new Scanner(System.in);
   for (int i=0;i<matriz.length ; i++ ) {
+      columna=sc.nextInt();
+      matriz[i][0]=columna;
       for (int j=0;j<matriz[i].length;j++ ) {
-
+      fila=sc.nextInt();
+      matriz[i][j]=fila;
       }
   }
 
 }//rellenaMatriz
 
+public void imprimeMatriz(){
+  int nueva[][]=this.getMatriz();
+  for (int i=0;i<nueva.length ;i++) {
+    for(int j=0; j<nueva[i].length;j++){
+      System.out.println("| " + nueva[i][j] + "|");
+    }
+  }
+}//imprimeMatriz
 
 //Metodos get  y set
 public int[][] getMatriz(){
@@ -31,6 +45,10 @@ public int[][] getMatriz(){
 
 public  void setMatriz(int[][] x){
     tamMatriz=x;
+}
+public static void main(String[] args) {
+  Matriz prueba= new Matriz(2);
+  prueba.rellenaMatriz();
 }
 
 }
