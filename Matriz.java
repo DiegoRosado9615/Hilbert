eccfc16fe892ba71d49cd8a10d607bfbb0c754ac
LoboStark <rocadie9615@ciencias.unam.mx>
2020-03-04 12:21:00 -0600

    Poducto escalar

--- Matriz.java
+++ Matriz.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class Matriz{
   int [][] tamMatriz;
-
 /**
 Recibe un entero que sera el tamanho de la
 matriz solo se crean matrizes cuadradas
@@ -19,8 +18,6 @@ public void rellenaMatriz(){
   int fila=0;
   Scanner sc= new Scanner(System.in);
   for (int i=0;i<matriz.length ; i++ ) {
-      columna=sc.nextInt();
-      matriz[i][0]=columna;
       for (int j=0;j<matriz[i].length;j++ ) {
       fila=sc.nextInt();
       matriz[i][j]=fila;
@@ -29,14 +26,35 @@ public void rellenaMatriz(){
 
 }//rellenaMatriz
 
+/**
+Metodo que imprime las matrizes
+*/
 public void imprimeMatriz(){
   int nueva[][]=this.getMatriz();
   for (int i=0;i<nueva.length ;i++) {
     for(int j=0; j<nueva[i].length;j++){
-      System.out.println("| " + nueva[i][j] + "|");
+      System.out.print("| " + nueva[i][j] + "|");
     }
+    System.out.println("");
   }
 }//imprimeMatriz
+
+/**
+Metodo con el que se hace el producto escalar recibe el escalar por el cual
+se va multiplicar
+@param Entero y
+@return Matriz
+*/
+public void productoEscalar(int x){
+  int [][] matriz=this.getMatriz();
+  int producto=0;
+  for (int i=0;i<matriz.length ;i++ ) {
+    for (int j=0;j<matriz[i].length ;j++) {
+      producto=matriz[i][j]*x;
+      matriz[i][j]=producto;
+    }
+  }
+}
 
 //Metodos get  y set
 public int[][] getMatriz(){
@@ -49,6 +67,8 @@ public  void setMatriz(int[][] x){
 public static void main(String[] args) {
   Matriz prueba= new Matriz(2);
   prueba.rellenaMatriz();
+  prueba.productoEscalar(2);
+  prueba.imprimeMatriz();
 }
 
 }
